dfad56bd117484e385db91208a11e8fbcf307ba9
CactusDuper <58598131+CactusDuper@users.noreply.github.com>
2020-06-18 09:21:06 -0500

    Update runme.py

--- runme.py
+++ runme.py
@@ -10,7 +10,7 @@ b = (-4824621 * upperBits + 7847617 * lowerBits + 7847617) >> 32
 seed = 7847617 * a - 18218081 * b
 if seed > 0:
     if(((seed * 25214903917 + 11) & ((1 << 48) - 1))) > 0:
-        nextLong = ((struct.unpack("@q",struct.pack("@Q",(seed >> 16 << 32)))[0]) + (((seed * 25214903917 + 11) & ((1 << 48) - 1)) >> 16))
+        nextLong = ((struct.unpack("@q",struct.pack("@Q",(seed >> 16 << 32)))[0]) + (((seed * 25214903917 + 11) & ((1 << 48) - 1)) >> 16)%-2147483648)
     else:
         nextLong = ((seed >> 16 << 32) + ((((seed * 25214903917 + 11) & ((1 << 48) - 1)) >> 16))+(2 <<15))
 else:
