844c43a3ec1c960a6687562177ccc4639c6c863a
CactusDuper <58598131+CactusDuper@users.noreply.github.com>
2020-06-18 09:48:25 -0500

    Update runme.py

--- runme.py
+++ runme.py
@@ -11,6 +11,7 @@ seed = 7847617 * a - 18218081 * b
 if seed > 0:
     if(((seed * 25214903917 + 11) & ((1 << 48) - 1))) > 0:
         nextLong = ((struct.unpack("@q",struct.pack("@Q",(seed >> 16 << 32)))[0]) + (((seed * 25214903917 + 11) & ((1 << 48) - 1)) >> 16)%-2147483648)
+        nextLong2 = ((struct.unpack("@q",struct.pack("@Q",(seed >> 16 << 32)))[0]) + (((seed * 25214903917 + 11) & ((1 << 48) - 1)) >> 16)%2147483648)
     else:
         nextLong = ((seed >> 16 << 32) + ((((seed * 25214903917 + 11) & ((1 << 48) - 1)) >> 16))+(2 <<15))
 else:
@@ -18,4 +19,10 @@ else:
         nextLong = (((((seed >> 16)+(2<<15)) << 32)) + (((seed * 25214903917 + 11) & ((1 << 48) - 1)) >> 16))
     else:
         nextLong = (((((seed >> 16)+(2<<15)) << 32)) + ((((seed * 25214903917 + 11) & ((1 << 48) - 1)) >> 16))+(2 <<15))
-print(nextLong==worldSeed)
+if(nextLong != worldSeed):
+    if(nextLong2 == worldSeed):
+        print("Valid Seed")
+    else:
+        print("Invalid Seed")
+elif(nextLong==worldSeed):
+    print("Valid Seed")
